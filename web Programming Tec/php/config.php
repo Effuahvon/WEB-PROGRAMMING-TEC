70d4cfdd499c1a5a7e43af73edfded3b970c3847
Ojoe <141376363+Alhassanojoekoroma@users.noreply.github.com>
2025-12-10 15:36:44 +0000

    Delete web Programming Tec directory

--- web Programming Tec/php/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Edit these values for your environment (XAMPP/MAMP/WAMP)
-define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'streetbull');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-
-// Recommended: restrict to local access or move credentials to environment variables
-?>
